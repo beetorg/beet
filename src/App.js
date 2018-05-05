4a783be295b5aa819cb5c31e711985b42ab74729
Austin Tindle <tindleaj@gmail.com>
2018-05-05 11:36:55 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Good
+      </div>
+    );
+  }
+}
+
+export default App;
